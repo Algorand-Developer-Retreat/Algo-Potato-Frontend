976654d8fbdae23f14040fd5097e541b4afcb3af
Eric Sharma <sharma.e@husky.neu.edu>
2025-04-02 17:57:37 +0200

    Added mainnet client

---
+++ lib/testnetAlgoClient.ts
@@ -0,0 +1,29 @@
+import { AlgorandClient, Config } from '@algorandfoundation/algokit-utils';
+import algosdk from 'algosdk';
+import { ClientManager } from '@algorandfoundation/algokit-utils/types/client-manager';
+
+Config.configure({
+  logger: Config.getLogger(true),
+});
+
+const currentNetwork: 'testnet' | 'mainnet' = 'testnet';
+
+export const indexer = ClientManager.getIndexerClient(
+  ClientManager.getAlgoNodeConfig(currentNetwork, 'indexer')
+);
+
+export const algod = ClientManager.getAlgodClient(
+  ClientManager.getAlgoNodeConfig(currentNetwork, 'algod')
+);
+
+export const kmd: algosdk.Kmd | undefined = ClientManager.getKmdClient({
+  server: 'http://localhost',
+  port: '4002',
+  token: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+});
+
+export const algorandClient = AlgorandClient.fromClients({
+  algod,
+  indexer,
+  kmd,
+});
