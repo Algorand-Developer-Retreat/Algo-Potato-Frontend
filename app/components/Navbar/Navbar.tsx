718831095f079646ca4da84bce703c9e4810c1d3
Eric Sharma <sharma.e@husky.neu.edu>
2025-04-02 18:10:39 +0200

    Lint fix

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
 import ConnectMenu from '../Txn-lab/Connect-menu';
 import { usePathname } from 'next/navigation';
 
